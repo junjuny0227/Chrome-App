7215146354804764922fd36d297f737452e3514c
junjuny0227 <junjuny0227@gmail.com>
2024-08-13 23:41:15 +0900

    making clock

---
+++ src/js/clock.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+  const date = new Date();
+  clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
