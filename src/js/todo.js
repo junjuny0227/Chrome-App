74f85bff37b0cac9744b1a7d42bb7f71c21d9a66
junjuny0227 <junjuny0227@gmail.com>
2024-08-17 19:48:44 +0900

    deleting ToDo button

--- src/js/todo.js
+++ src/js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
+  li.appendChild(button);
   span.innerText = newToDo;
   toDoList.appendChild(li);
 }
