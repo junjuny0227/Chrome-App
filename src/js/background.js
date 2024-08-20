5da7922ce1d77c9dea80f23d109315deb243b5b9
junjuny0227 <junjuny0227@gmail.com>
2024-08-20 17:15:28 +0900

    Error Correction

--- src/js/background.js
+++ src/js/background.js
@@ -1,20 +1,10 @@
-const images = [
-  "CherryGrove.png",
-  "Desert.png",
-  "FlowerForest.png",
-  "Jungle.png",
-  "Savanna.png",
-  "FrozenRiver.png",
-  "GravellyHills.png",
-  "MangroveSwamp.png",
-  "MushroomFields.png",
-];
+const images = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `/img/${chosenImage}`;
-bgImage.style.width = "62%";
+bgImage.src = `/img/${chosenImage}.jpg`;
+bgImage.style.width = "50%";
 
 document.body.appendChild(bgImage);
