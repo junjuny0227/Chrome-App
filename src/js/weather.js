92d655f9f66dd396462ff6737768d386c7345dd7
junjuny0227 <junjuny0227@gmail.com>
2024-08-20 23:48:13 +0900

    Almost done

---
+++ src/js/weather.js
@@ -0,0 +1,20 @@
+import keys from "./keys.js";
+
+const API_KEY = keys.WEATHER_API_KEY;
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const country = data.sys.country;
+      const weather = data.weather[0].main;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
